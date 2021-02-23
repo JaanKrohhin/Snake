524309f201cccc55c8c0a4aa814932d0422ea88c
Jaan Krohhin <jaan.krohin@gmail.com>
2021-02-23 08:25:55 +0200

    +

--- Snek_ssssssss/Program.cs
+++ Snek_ssssssss/Program.cs
@@ -3,50 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Snek_ssssssss
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.SetWindowSize(80, 25);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(80, 25);
 
-            Walls walls = new Walls(80, 25);
-            walls.Draw();
-             
-            Point p = new Point(4, 5, '*');
-            Snake snake=new Snake(p,4,Directions.RIGHT);
-            snake.Draw();
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
-            while (true)
-            {
-                if (walls.IsHit(snake) || snake.IsHitTail())
-                {
-                    break;
-                }
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
-                Thread.Sleep(100);
-                
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
-            }
-        }
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Directions.RIGHT);
+			snake.Draw();
 
-    }
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("G A M E	O V E R", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Autor: Evgenij Kartavec", xOffset + 2, yOffset++);
+			WriteText("Made for GeekBrains", xOffset + 1, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
 }
